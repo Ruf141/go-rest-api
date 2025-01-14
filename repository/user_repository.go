4e959141bbe08d2f16cc7e1bf7bb1c81a738d4a8
Ruf141 <mad0908141@gmail.com>
2025-01-14 22:53:09 +0900

    fix user repo

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,8 +1,26 @@
 package repository
 
-import "go-rest-api/model"
+import (
+	"go-rest-api/model"
+
+	"gorm.io/gorm"
+)
 
 type IUserRepository interface {
 	GetUserByEmail(user *model.User, email string) error
 	CreateUser(user *model.User) error
+}
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository{db}
+}
+
+func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
+	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
+		return err
+	}
+	return nil
 }
