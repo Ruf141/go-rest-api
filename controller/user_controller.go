da06ce34a2117bd9c95acece383e181299059ab0
Ruf141 <mad0908141@gmail.com>
2025-01-17 23:45:29 +0900

    add user_controller to login

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -4,6 +4,8 @@ import (
 	"go-rest-api/model"
 	"go-rest-api/usecase"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -32,4 +34,25 @@ func (uc *userController) SignUp(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusCreated, userRes)
+}
+
+func (uc *userController) Login(c echo.Context) error {
+	user := model.User{}
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	tokenString, err := uc.uu.Login(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "token"
+	cookie.Value = tokenString
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	cookie.Path = "/"
+	cookie.Domain = os.Getenv("API_DOMAIN")
+	cookie.HttpOnly = true
+	cookie.SameSite = http.SameSiteNoneMode
+	c.SetCookie(cookie)
+	return c.NoContent(http.StatusOK)
 }
