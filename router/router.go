e4c5da1be2be5914e7363b56ea5d3e3ec71f11bf
Ruf141 <mad0908141@gmail.com>
2025-01-18 23:56:37 +0900

    add main.go

--- router/router.go
+++ router/router.go
@@ -10,6 +10,6 @@ func NewRouter(uc controller.IUserController) *echo.Echo {
 	e := echo.New()
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
-	e.POST("/signup", uc.LogOut)
+	e.POST("/logout", uc.LogOut)
 	return e
 }
