a3ec040cc8616630b632b440ca9ed746b144cd3b
Ruf141 <mad0908141@gmail.com>
2025-01-24 23:32:46 +0900

    add delete task

--- repository/task_repository.go
+++ repository/task_repository.go
@@ -29,6 +29,20 @@ func (tr *taskRepository) GetAllTasks(tasks *[]model.Task, userId uint) error {
 	return nil
 }
 
+func (tu *taskUsecase) GetTaskById(userId uint, taskId uint) (model.TaskResponse, error) {
+	task := model.Task{}
+	if err := tu.tr.GetTaskById(&task, userId, taskId); err != nil {
+		return model.TaskResponse{}, err
+	}
+	resTask := model.TaskResponse{
+		ID:        taskId,
+		Title:     task.Title,
+		CreatedAt: task.CreatedAt,
+		UpdatedAt: task.UpdatedAt,
+	}
+	return resTask, nil
+}
+
 func (tu *taskUsecase) CreateTask(task model.Task) (mode.TaskResponse, error) {
 	if err := tu.tr.CreateTask(&task); err != nil {
 		return model.TaskResponse{}, err
@@ -40,4 +54,11 @@ func (tu *taskUsecase) CreateTask(task model.Task) (mode.TaskResponse, error) {
 		UpdatedAt: task.UpdatedAt,
 	}
 	return resTask, nil
+}
+
+func (tu *taskUsecase) DeliteTask(userId uint, taskId uint) error {
+	if err := tu.tr.DeliteTask(userId, taskId); err != nil {
+		return err
+	}
+	return nil
 }
